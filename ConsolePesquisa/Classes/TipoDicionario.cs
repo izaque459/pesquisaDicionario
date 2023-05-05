de78fb209a77a3683f6b5fe2fa6d2ef946b6f56c
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-05 14:33:25 -0300

    Adicionado metodo Insere na classe tipoDicionario

--- ConsolePesquisa/Classes/TipoDicionario.cs
+++ ConsolePesquisa/Classes/TipoDicionario.cs
@@ -11,4 +11,17 @@ public class TipoDicionario
     {
         n = NumReg.Minimo;
     }
+
+    public void Insere(Registro reg)
+    {
+        if (n == NumReg.Maximo)
+        {
+            Console.WriteLine("Erro: Dicionario cheio");
+        }
+        else
+        {
+            n++;
+            Item[n] = reg;
+        }
+    }
 }
