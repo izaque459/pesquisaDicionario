a94831c79d285fbe828b06cf455e35afa1d3f02e
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-09 19:44:28 -0300

    Adicionado ao programa principal um vetor nao ordenado e outro ordenado

--- ConsolePesquisa/Program.cs
+++ ConsolePesquisa/Program.cs
@@ -6,7 +6,20 @@ namespace ConsolePesquisa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vetorNaoOrdenado = new int[1000];
+            Random rnd = new Random();
+            for (int i = 0; i < vetorNaoOrdenado.Length; i++)
+            {
+                vetorNaoOrdenado[i] = rnd.Next(1, 1051);
+            }
+
+
+            int[] vetorOrdenado = new int[1000];
+            for (int i = 0; i < vetorOrdenado.Length; i++)
+            {
+               vetorOrdenado[i] = i + 1;
+            }
+
         }
     }
 }
