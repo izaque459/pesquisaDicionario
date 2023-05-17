732133d29a095ace3a8984af48f924e83f988f18
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-17 16:53:24 -0300

    Revert " Refatorado e corrigido semantica em Program para aceitar o dicionario"
    This reverts commit 970b3c10e0839e58051a14c37b8996016f229c8b.

--- ConsolePesquisa/Program.cs
+++ ConsolePesquisa/Program.cs
@@ -6,34 +6,16 @@ namespace ConsolePesquisa
     {
         static void Main(string[] args)
         {
-            TipoDicionario dicionarioNaoOrdenado = new TipoDicionario();
-            dicionarioNaoOrdenado.Inicializa();
+            int[] vetorNaoOrdenado = new int[1000];
             Random rnd = new Random();
-            for (int i = 1; i < NumReg.Maximo; i++)
+            for (int i = 0; i < vetorNaoOrdenado.Length; i++)
             {
-                dicionarioNaoOrdenado.Insere(rnd.Next(1, 1051));
+                vetorNaoOrdenado[i] = rnd.Next(1, 1051);
             }
 
 
-            
-            if (args.Length == 0)
-            {
-                 Console.WriteLine("Nenhum argumento foi fornecido.");
-            }else{
-                int passosLinear, passosBinaria;
+            int[] vetorOrdenado = vetorNaoOrdenado;
 
-                int posicaoLinear = TipoPesquisa.Linear(out dicionarioNaoOrdenado, out passosLinear, out elemento);
-                Console.WriteLine("Pesquisa Linear:");
-                if (posicaoLinear != -1)
-                {
-                    Console.WriteLine("Elemento encontrado na posição {0}.", posicaoLinear);
-                }
-                else
-                {
-                    Console.WriteLine("Elemento não encontrado.");
-                }
-                Console.WriteLine("Número de passos: {0}", passosLinear);
-
-            }
+            Array.sort(vetorOrdenado);
     }
 }
