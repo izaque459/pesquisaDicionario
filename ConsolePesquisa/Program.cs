56a06eabff7dff4a76652a95948af58039f661bb
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-17 16:56:25 -0300

    Revert " Adicionado ao programa principal um vetor nao ordenado e outro ordenado"
    This reverts commit a94831c79d285fbe828b06cf455e35afa1d3f02e.

--- ConsolePesquisa/Program.cs
+++ ConsolePesquisa/Program.cs
@@ -6,20 +6,7 @@ namespace ConsolePesquisa
     {
         static void Main(string[] args)
         {
-            int[] vetorNaoOrdenado = new int[1000];
-            Random rnd = new Random();
-            for (int i = 0; i < vetorNaoOrdenado.Length; i++)
-            {
-                vetorNaoOrdenado[i] = rnd.Next(1, 1051);
-            }
-
-
-            int[] vetorOrdenado = new int[1000];
-            for (int i = 0; i < vetorOrdenado.Length; i++)
-            {
-               vetorOrdenado[i] = i + 1;
-            }
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
