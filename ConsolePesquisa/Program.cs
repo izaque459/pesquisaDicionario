c8871bf9fe58167588b39fd2a167c254bb0b0420
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-22 16:34:38 -0300

    Adicionado funcao PesquisaLinear

--- ConsolePesquisa/Program.cs
+++ ConsolePesquisa/Program.cs
@@ -14,6 +14,25 @@ namespace ConsolePesquisa
         {
             dicionario.elementos = new string[dicionario.tamanho];
         }
+
+        public static void Insere(ref TipoDicionario dicionario, int posicao, string elemento)
+        {
+            dicionario.elementos[posicao] = elemento;
+        }
+
+        public static int PesquisaLinear(TipoDicionario dicionario, string elemento, out int passos)
+        {
+            passos = 0;
+            for (int i = 0; i < dicionario.tamanho; i++)
+            {
+                passos++;
+                if (dicionario.elementos[i] == elemento)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
