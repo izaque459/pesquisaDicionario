4c1888674855ce36675994af6db013563ef731d6
Isaac Santos <isaacdsantos@hotmail.com>
2023-05-22 18:22:23 -0300

    Reformulado funcao Inicializar

--- ConsolePesquisa/Program.cs
+++ ConsolePesquisa/Program.cs
@@ -10,14 +10,17 @@ namespace ConsolePesquisa
             public int tamanho;
         }
 
-        public static void Inicializar(ref TipoDicionario dicionario)
+        public static void Inicializar(out TipoDicionario dicionario)
         {
-            dicionario.elementos = new string[dicionario.tamanho];
+                       dicionario = new TipoDicionario();
+                       dicionario.elementos = new string[100];
+                       dicionario.tamanho = 0;
         }
 
         public static void Insere(ref TipoDicionario dicionario, int posicao, string elemento)
         {
             dicionario.elementos[posicao] = elemento;
+            dicionario.tamanho++;
         }
 
         public static int PesquisaLinear(TipoDicionario dicionario, string elemento, out int passos)
@@ -35,19 +38,42 @@ namespace ConsolePesquisa
         }
         static void Main(string[] args)
         {
+            TipoDicionario dicionario;
+            
+            Inicializar(out dicionario);
+
+            // Adicionar elementos ao dicionário
+            Insere(ref dicionario, 0, "abc");
+            Insere(ref dicionario, 1, "def");
+            Insere(ref dicionario, 2, "ghi");
+            Insere(ref dicionario, 3, "jkl");
+
+
+
             if (args.Length == 0)
             {
                 Console.WriteLine("Nenhum argumento foi fornecido. Pesquisa não realizada.");
             }
             else
             {
+                int passosLinear, passosBinaria;
                 Console.WriteLine("Argumentos fornecidos:");
                 foreach (string argumento in args)
                 {
                     Console.WriteLine(argumento);
+                    int posicaoLinear = PesquisaLinear(dicionario, argumento, out passosLinear);
+                    Console.WriteLine("Pesquisa Linear:");
+                    if (posicaoLinear != -1)
+                    {
+                        Console.WriteLine("Elemento encontrado na posição {0}.", posicaoLinear);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Elemento não encontrado.");
+                    }
+                    Console.WriteLine("Número de passos: {0}", passosLinear);
+                                    
                 }
-
-                int passosLinear, passosBinaria;
             }
         }
     }
